f5b0b5cea484dc1fff05c318210ad1c9936b8e88
McMadloi <moelya95@gmail.com>
2025-07-03 12:36:37 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { createClient } = require('@libsql/client');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const { createClient } = require('redis')
 const path = require('path');
 const helmet = require('helmet');
 require('dotenv').config();
